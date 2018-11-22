421e3249ddd3751de15d6fdb589fcccaf53acd24
Kolodiazhnyi Serhii <elkrot@gmail.com>
2018-11-22 13:08:49 +0200

    first commit

---
+++ elkrotbot/elkrotbot/Models/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBotApp.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; }  = "https://elkrotbot.azurewebsites.net:443/{0}";
+
+        public static string Name { get; set; } = "elkrotbot";
+
+        public static string Key { get; set; }  = "764370336:AAEay1tQQodk4TpFHQ-nYtylP9hLGsnVVKE";
+
+    }
+}
